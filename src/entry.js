35bc81bce76fd087e8d4dd1f55675ce18102b067
guokeke <1040707795@qq.com>
2018-04-09 20:27:59 +0800

    保留src文件

---
+++ src/entry.js
@@ -0,0 +1,10 @@
+import css from './css/index.css'
+import less from './css/black.less'
+import sass from './css/aa.scss'
+
+
+{
+  let info = 'Hello world!'
+  document.getElementById('app').innerHTML = info
+}
+$('#app').html('郭可可真帅')
